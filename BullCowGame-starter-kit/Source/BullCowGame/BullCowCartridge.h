14b5f44b4c8386d3539daaee700f1445688087e7
Josh Hehir <67282489+joshhehir@users.noreply.github.com>
2020-12-16 14:52:25 +1100

    Completed game

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -6,6 +6,12 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
@@ -15,8 +21,15 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 	void SetupGame();
+	void EndGame();
+	void ProcessGuess(const FString& Guess);
+	bool IsIsogram(const FString& Word) const;
+	TArray<FString> GetValidWords(const TArray<FString>& WordList) const;
+	FBullCowCount GetBullCows(const FString& Guess) const;
 
 	private:
 	FString HiddenWord;
 	int32 Lives;
+	bool bGameOver;
+	TArray<FString> Isograms;
 };
